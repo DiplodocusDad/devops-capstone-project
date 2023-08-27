f2f0a79c30e0094be4e3494eb1a96c5006b3e920
ThreeFlightlessBirds <thommorley@gmail.com>
2023-08-27 19:38:19 -0400

    added test_read_an_account and started get_accounts

--- service/routes.py
+++ service/routes.py
@@ -68,7 +68,15 @@ def create_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
-# ... place you code here to READ an account ...
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def get_accounts(account_id):
+    """
+    
+    """
+    app.logger.info("Request to read an Account with id: %s", account_id)
+
+    account = Account.find(account_id)
+    
 
 
 ######################################################################
